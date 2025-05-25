5b4e7590884cf29488e59b5537291b788b9e2bf7
nazia81 <160647694+nazia81@users.noreply.github.com>
2025-05-26 03:30:07 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print(f"Name: {self.name}, Marks: {self.marks}")
+
+if __name__ == "__main__":
+    student1 = Student("nazia", 90)
+    student1.display()
+
+
+
+
+
